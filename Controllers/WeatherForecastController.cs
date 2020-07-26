32394ad4498c09a69a147cf675fb34e58b4b72ed
Mohit Kapadia <mohit.kapadia@hotmail.com>
2020-07-26 14:26:33 +1200

    added new property

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace WeatherForecast.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Code = "Test new image"
             })
             .ToArray();
         }
